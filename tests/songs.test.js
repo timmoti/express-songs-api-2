6df653f5d313ef3ca025c91a97317fd6f2824dba
Angeline <atan@thoughtworks.com>
2018-10-11 19:17:27 +0800

    Add in the empty test case for songs API to be filled in by students

---
+++ tests/songs.test.js
@@ -0,0 +1,34 @@
+const app = require("../app");
+const request = require("supertest");
+
+//Fill in the test case below for the Songs API
+
+describe("routes/songs", () => {
+
+  it("POST /songs should return a new song object", () => {
+   
+
+  });
+  
+  it("GET /songs should return a non empty array", () => {
+   
+
+  });
+  
+  it("PUT /songs should return the updated song", () => {
+    
+
+  });
+
+  it("DELETE /songs/:id should return the deleted song", () => {
+    
+
+  });
+  
+  it("GET /songs should return an empty array", () => {
+    
+    
+  });
+
+});
+

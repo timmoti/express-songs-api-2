a9ddf646b8265668643d15b73e7428b44fe670bf
Angeline <atan@thoughtworks.com>
2018-10-12 01:28:52 +0800

    Add scenario where there are CRUD routes for songs and movies in app.js

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 let songs = [];
-let id = 0;
+let movies = [];
+let songID = 0;
+let movieID = 0;
 
+//Song API
 //return list of all songs
 app.get('/songs', (req, res) => {
   res.status(200).json(songs);
@@ -18,8 +21,8 @@ app.get('/songs', (req, res) => {
 //create a new song, and return new song
 app.post('/songs', (req, res) => {
   let song = req.body;
-  id++;
-  song.id = id + '';
+  songID++;
+  song.id = songID + '';
   songs.push(song);
   res.status(201).json(song);
 });
@@ -46,6 +49,41 @@ app.delete("/songs/:id", (req, res) => {
   let songToDelete = songs[_.findIndex(songs, {id: req.params.id})];
   songs = songs.filter(song => song.id !== req.params.id);
   res.status(200).json(songToDelete);
+});
+
+
+// Movies API
+app.get('/movies', (req, res) => {
+  res.status(200).json(movies);
+});
+
+app.post('/movies', (req, res) => {
+  let movie = req.body;
+  movieID++;
+  movie.id = movieID + '';
+  movies.push(movie);
+  res.status(201).json(movie);
+});
+
+app.get('/movies/:id', (req, res) => {
+  let movie = _.find(movies, {id: req.params.id});
+  res.status(200).json(movie || {});
+});
+
+app.put('/movies/:id', (req, res) => {
+  let update = req.body;
+  if (update.id) {
+    delete update.id
+  }
+  let movie = _.findIndex(movies, {id: req.params.id});
+  let updatedMovie = _.assign(movies[movie], update);
+  res.status(200).json(updatedMovie);
+});
+
+app.delete("/movies/:id", (req, res) => {
+  let movieToDelete = movies[_.findIndex(movies, {id: req.params.id})];
+  movies = movies.filter(movie => movie.id !== req.params.id);
+  res.status(200).json(movieToDelete);
 });
 
 module.exports = app
---
+++ tests/movies.test.js
@@ -0,0 +1,73 @@
+const app = require("../app");
+const request = require("supertest");
+
+describe("routes/movies", () => {
+
+  it("POST /movies should return a new movie object", () => {
+    requestBody = { name: "test movie", artist: "rhianna"};
+    
+    return request(app)
+    .post("/movies")
+    .send(requestBody)
+    
+    .then(response => {
+      expect(response.status).toEqual(201);
+      expect(response.body).toMatchObject(requestBody);
+    });
+  });
+  
+  it("GET /movies should return a non empty array", () => {
+    return request(app)
+    .get("/movies")
+    
+    .then(response => {
+      expect(response.status).toEqual(200);
+      expect(Array.isArray(response.body)).toEqual(true);
+      expect(response.body.length).toBeGreaterThan(0);
+    });
+  });
+  
+  it("PUT /movies should return the updated movie", () => {
+    requestBody = {
+      name: "updated movie",
+      artist: "rhianna"
+    };
+    
+    return request(app)
+    .put("/movies/1")
+    .send(requestBody)
+    
+    .then(response => {
+      expect(response.status).toEqual(200);
+      expect(response.body).toMatchObject(requestBody);
+    });
+  });
+
+  it("DELETE /movies/:id should return the deleted movie", () => {
+    const ID = 1;
+    expected = {
+      name: "updated movie",
+      artist: "rhianna"
+    };
+
+    return request(app)
+    .delete("/movies/1")
+
+    .then(response => {
+      expect(response.status).toEqual(200);
+      expect(response.body).toMatchObject(expected);
+    })
+  });
+  
+  it("GET /movies should return an empty array", () => {
+    return request(app)
+    .get("/movies")
+    
+    .then(response => {
+      expect(response.status).toEqual(200);
+      expect(Array.isArray(response.body)).toBe(true);
+      expect(response.body.length).toEqual(0);
+    });
+  });
+
+});

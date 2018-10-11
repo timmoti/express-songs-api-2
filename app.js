f8352f637faaa4ffba7638e5a87fd1f1205ac1a8
Angeline <atan@thoughtworks.com>
2018-10-11 17:08:30 +0800

    Initial CRUD app with routes and controllers to be filled in by students

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const _ = require('lodash');
+const PORT = 3000
+
+app.use(express.static('client'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+let songs = [];
+let id = 0;
+
+//return list of all songs
+
+
+//create a new song, and return new song
+
+
+//return a song with id 
+
+
+//edit a song with id, and return edited song
+
+
+//delete a song with id, and return deleted song
+
+
+app.listen(PORT);
+console.log(`Server listening on port ${PORT}`);

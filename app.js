7575d612a686aea2f2a6d6345698474f27424c95
Angeline <atan@thoughtworks.com>
2018-10-11 19:16:42 +0800

    Split listen into server.js so that test case will not hang

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const _ = require('lodash');
-const PORT = 3000
 
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,7 +12,7 @@ let id = 0;
 
 //return list of all songs
 app.get('/songs', (req, res) => {
-  res.json(songs);
+  res.status(200).json(songs);
 });
 
 //create a new song, and return new song
@@ -22,13 +21,13 @@ app.post('/songs', (req, res) => {
   id++;
   song.id = id + '';
   songs.push(song);
-  res.json(song);
+  res.status(201).json(song);
 });
 
 //return a song with id 
 app.get('/songs/:id', (req, res) => {
   let song = _.find(songs, {id: req.params.id});
-  res.json(song || {});
+  res.status(200).json(song || {});
 });
 
 //edit a song with id, and return edited song
@@ -39,15 +38,14 @@ app.put('/songs/:id', (req, res) => {
   }
   let song = _.findIndex(songs, {id: req.params.id});
   let updatedSong = _.assign(songs[song], update);
-  res.json(updatedSong);
+  res.status(200).json(updatedSong);
 });
 
 //delete a song with id, and return deleted song
 app.delete("/songs/:id", (req, res) => {
   let songToDelete = songs[_.findIndex(songs, {id: req.params.id})];
   songs = songs.filter(song => song.id !== req.params.id);
-  res.json(songToDelete);
+  res.status(200).json(songToDelete);
 });
 
-app.listen(PORT);
-console.log(`Server listening on port ${PORT}`);
+module.exports = app

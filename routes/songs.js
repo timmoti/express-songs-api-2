ce268b082259b5710cb738b2de64b957c5d9f2cc
Angeline <atan@thoughtworks.com>
2018-10-12 16:54:56 +0800

    Add error handling for songs API
    - create functions to getSongs, getSong, createSong, updateSong, deleteSong to return a promise simulating mongoose
    - refactor controllers to use async and await with these functions
    - refactor controllers to return error response if error occurs
    - add tests to test these error scenarios

--- routes/songs.js
+++ routes/songs.js
@@ -5,43 +5,116 @@ const _ = require('lodash');
 let songs = [];
 let songID = 0;
 
+const createPromise = new Promise((resolve, reject) => {
+    setTimeout(function() {
+      resolve();
+    }, 300);
+  });
+
+const getSongs = (resolve, reject) => {
+    return createPromise.then(() => {
+        return songs;
+    });
+}
+
+const createSong = (song, reject) => {
+    return createPromise.then(()=>{
+        songs.push(song);
+        return song;
+    });
+}
+
+const getSong = (id, reject) => {
+    return createPromise.then(() => {
+        let songFound = _.find(songs, {id: id});
+        if (!songFound){
+            throw new Error("Id not found")
+        }
+        return songFound;
+    });
+}
+
+const updateSong = (songToUpdate, reject) => {
+    return createPromise.then(() => {
+        let id = songToUpdate.id
+        let song = _.findIndex(songs, {id: id});
+        if(song == -1) {
+            throw new Error()
+        }
+        let updatedSong = _.assign(songs[song], songToUpdate);
+        return updatedSong
+    });
+}
+
+const deleteSong = (songToDeleteID, reject) => {
+    return createPromise.then(() => { 
+        let songToDelete = songs[_.findIndex(songs, {id: songToDeleteID})];
+        if(!songToDelete) {
+            throw new Error()
+        }
+        songs = songs.filter(song => song.id !== songToDeleteID);
+        return songToDelete;
+    });
+}
+
 //Song API
 //return list of all songs
-router.get('/', (req, res) => {
-    res.status(200).json(songs);
-  });
-  
-//create a new song, and return new song
-router.post('/', (req, res) => {
-    let song = req.body;
-    songID++;
-    song.id = songID + '';
-    songs.push(song);
-    res.status(201).json(song);
+router.get('/', async (req, res) => {
+    try{
+        let allsongs = await getSongs();
+        res.status(200).json(songs);
+    }
+    catch(error){
+        throw error;
+        //res.status(404).json({message: "unable to list all songs"})
+    }
 });
+  
+router.post('/', async (req, res) => {
+    try{
+        let song = req.body;
+        songID++;
+        song.id = songID + '';
+        newSong = await createSong(song);
+        res.status(201).json(newSong);
+    }
+    catch(error){
+        res.status(404).json({message: "Unable to create new song" })
+    }
+  });
 
 //return a song with id 
-router.get('/:id', (req, res) => {
-    let song = _.find(songs, {id: req.params.id});
-    res.status(200).json(song || {});
+router.get('/:id', async (req, res) => {
+    try {
+        let songID = req.params.id;
+        let song = await getSong(songID)
+        res.status(200).json(song);
+    }
+    catch(error){
+        res.status(404).json({message: `Unable to find song with id: ${req.params.id}` })
+    }
 });
 
 //edit a song with id, and return edited song
-router.put('/:id', (req, res) => {
-    let update = req.body;
-    if (update.id) {
-        delete update.id
+router.put('/:id', async (req, res) => {
+    try{
+        let song = await updateSong(req.body)
+        res.status(200).json(song);
     }
-    let song = _.findIndex(songs, {id: req.params.id});
-    let updatedSong = _.assign(songs[song], update);
-    res.status(200).json(updatedSong);
+    catch(error){
+        res.status(404).json({message: `Unable to update song with id: ${req.params.id}` })
+    }
 });
 
 //delete a song with id, and return deleted song
-router.delete("/:id", (req, res) => {
-    let songToDelete = songs[_.findIndex(songs, {id: req.params.id})];
-    songs = songs.filter(song => song.id !== req.params.id);
-    res.status(200).json(songToDelete);
+router.delete("/:id", async (req, res) => {
+    try{
+        let songDeleted = await deleteSong(req.params.id)
+        res.status(200).json(songDeleted);
+    }
+    catch(error){
+        res.status(404).json({message: `Unable to delete song with id: ${req.params.id}` })
+    }
 });
 
 module.exports = router;

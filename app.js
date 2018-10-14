5a11c9f9d4976567c7efe5f0c51eb7110c597c01
Angeline <atan@thoughtworks.com>
2018-10-14 21:53:54 +0800

    Update app.js to use express.json() instead of bodyparser as it is now builtin to 4.16.x

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const songRouter = require("./routes/songs");
 const movieRouter = require("./routes/movies");
 
 app.use(express.static('client'));
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(express.json())
 
 app.use('/songs', songRouter);
 app.use('/movies', movieRouter);

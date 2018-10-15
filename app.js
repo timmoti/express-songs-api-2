c396ce2f98b828ee726c61cb14a616e1aadf6074
Angeline <atan@thoughtworks.com>
2018-10-15 12:04:12 +0800

    Extract id param from url and find song object

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ app.use(express.json())
 
 let songs = [];
 
+app.param('id', (req, res, next, id) => {
+  let song = songs.find(song => song.id === parseInt(id));
+  if (song) {
+    req.song = song;
+    next();
+  } 
+  else 
+  {
+    res.status(404).json({message : "Resource not found"});
+  }
+});
+
 //return list of all songs
 app.get('/songs', (req, res) => {
   res.status(200).json(songs);
@@ -24,21 +36,19 @@ app.post('/songs', (req, res) => {
 
 //return a song with id 
 app.get('/songs/:id', (req, res) => {
-  let song = songs.find(song => song.id == parseInt(req.params.id));
-  res.status(200).json(song);
+  res.status(200).json(req.song);
 });
 
 //edit a song with id, and return edited song
 app.put('/songs/:id', (req, res) => {
-  let song = songs.find(song => song.id === parseInt(req.params.id));
-  song.name = req.body.name;
-  song.artist = req.body.artist;
-  res.status(200).json(song);
+  req.song.name = req.body.name;
+  req.song.artist = req.body.artist;
+  res.status(200).json(req.song);
 });
 
 //delete a song with id, and return deleted song
 app.delete("/songs/:id", (req, res) => {
-  let songToDelete = songs.find(song => song.id === parseInt(req.params.id));
+  let songToDelete = req.song
   let index = songs.indexOf(songToDelete);
   songs.splice(index, 1);
   res.status(200).json(songToDelete);
